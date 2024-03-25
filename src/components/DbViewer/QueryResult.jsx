75d54ec1244c60cbb54f75fa2dee825ce0de5aca
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-03-24 18:18:28 -0700

    converted QueryResult and Toolbar into functional components

--- src/components/DbViewer/QueryResult.jsx
+++ src/components/DbViewer/QueryResult.jsx
@@ -1,79 +1,75 @@
-import { Component } from 'react';
 import { TableVirtuoso } from 'react-virtuoso';
 import PropTypes from 'prop-types';
 import { transformQueryResult } from '../../utils/transform';
 
-export class QueryResult extends Component {
-  render() {
-    const { queryResult, loadingResult, queryMessage } = this.props;
+export const QueryResult = ({ queryResult, loadingResult, queryMessage }) => {
+  const res = queryResult.length
+    ? queryResult[0]
+    : { columns: [], values: [] };
 
-    const res = queryResult.length
-      ? queryResult[0]
-      : { columns: [], values: [] };
-
-    return (
-      <div>
-        <h5 className="my-2">Result</h5>
-        <div style={{ overflow: 'scroll', height: 600 }}>
-          {loadingResult ? (
-            <div>Fetching results...</div>
-          ) : (
-            <>
-              {queryMessage === 'select' ? (
-                <>
-                  {queryResult.length ? (
-                    <div className="table-responsive">
-                      <TableVirtuoso
-                        style={{ height: 600 }}
-                        data={transformQueryResult(res)}
-                        totalCount={res.values.length}
-                        components={{
-                          // eslint-disable-next-line react/prop-types
-                          Table: ({ children }) => {
-                            return (
-                              <table className="table table-striped table-hover table-bordered">
-                                {children}
-                              </table>
-                            );
-                          }
-                        }}
-                        fixedHeaderContent={() => (
-                          <tr>
-                            {res.columns.map(col => (
-                              <th key={col + Math.random()}>{col}</th>
-                            ))}
-                          </tr>
-                        )}
-                        itemContent={(index, data) => (
-                          <>
-                            {res.columns.map(col => (
-                              <td key={data[col] + Math.random()}>
-                                {data[col]}
-                              </td>
-                            ))}
-                          </>
-                        )}
-                      />
-                    </div>
-                  ) : (
-                    <div>This query returned no results</div>
-                  )}
-                </>
-              ) : (
-                <div className="alert alert-success">
-                  <p>{queryMessage}</p>
-                </div>
-              )}
-            </>
-          )}
-        </div>
+  return (
+    <div>
+      <h5 className="my-2">Result</h5>
+      <div style={{ overflow: 'scroll', height: 600 }}>
+        {loadingResult ? (
+          <div>Fetching results...</div>
+        ) : (
+          <>
+            {queryMessage === 'select' ? (
+              <>
+                {queryResult.length ? (
+                  <div className="table-responsive">
+                    <TableVirtuoso
+                      style={{ height: 600 }}
+                      data={transformQueryResult(res)}
+                      totalCount={res.values.length}
+                      components={{
+                        // eslint-disable-next-line react/prop-types
+                        Table: ({ children }) => {
+                          return (
+                            <table className="table table-striped table-hover table-bordered">
+                              {children}
+                            </table>
+                          );
+                        }
+                      }}
+                      fixedHeaderContent={() => (
+                        <tr>
+                          {res.columns.map(col => (
+                            <th key={col + Math.random()}>{col}</th>
+                          ))}
+                        </tr>
+                      )}
+                      itemContent={(index, data) => (
+                        <>
+                          {res.columns.map(col => (
+                            <td key={data[col] + Math.random()}>
+                              {data[col]}
+                            </td>
+                          ))}
+                        </>
+                      )}
+                    />
+                  </div>
+                ) : (
+                  <div>This query returned no results</div>
+                )}
+              </>
+            ) : (
+              <div className="alert alert-success">
+                <p>{queryMessage}</p>
+              </div>
+            )}
+          </>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 QueryResult.propTypes = {
   queryResult: PropTypes.array.isRequired,
   loadingResult: PropTypes.bool.isRequired,
   queryMessage: PropTypes.string.isRequired
 };
+
--- src/components/DbViewer/Toolbar.jsx
+++ src/components/DbViewer/Toolbar.jsx
@@ -1,75 +1,71 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-export class Toolbar extends Component {
-  fontSizes = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
+export const Toolbar = ({
+  executeQuery,
+  setQuery,
+  setFontSize,
+  setWordWrap,
+  wordWrap,
+  fontSize
+}) => {
+  const fontSizes = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
-  render() {
-    const {
-      executeQuery,
-      setQuery,
-      setFontSize,
-      setWordWrap,
-      wordWrap,
-      fontSize
-    } = this.props;
-    return (
-      <div className="d-flex">
-        <div className="d-flex justify-content-start align-items-start mt-2">
-          <div className="form-check form-switch me-2 bg-light-tertiary">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="word-wrap"
-              checked={wordWrap === 'on'}
-              onChange={() => setWordWrap(wordWrap === 'on' ? 'off' : 'on')}
-            />
-            <label className="form-check-label" htmlFor="word-wrap">
-              Word Wrap
-            </label>
-          </div>
-        </div>
-
-        <div className="me-2">
-          <button
-            type="button"
-            className="btn btn-light dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Font size: {fontSize}
-          </button>
-          <ul className="dropdown-menu">
-            {this.fontSizes.map(size => (
-              <li key={size} onClick={() => setFontSize(size)}>
-                <span className="dropdown-item">{size}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <button
-            type="button"
-            className="btn btn-danger btn-sm mx-1"
-            onClick={() => setQuery('')}
-          >
-            Clear
-          </button>
-
-          <button
-            type="button"
-            className="btn btn-success btn-sm mx-1"
-            onClick={executeQuery}
-          >
-            Run
-          </button>
+  return (
+    <div className="d-flex">
+      <div className="d-flex justify-content-start align-items-start mt-2">
+        <div className="form-check form-switch me-2 bg-light-tertiary">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="word-wrap"
+            checked={wordWrap === 'on'}
+            onChange={() => setWordWrap(wordWrap === 'on' ? 'off' : 'on')}
+          />
+          <label className="form-check-label" htmlFor="word-wrap">
+            Word Wrap
+          </label>
         </div>
       </div>
-    );
-  }
-}
+
+      <div className="me-2">
+        <button
+          type="button"
+          className="btn btn-light dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Font size: {fontSize}
+        </button>
+        <ul className="dropdown-menu">
+          {fontSizes.map(size => (
+            <li key={size} onClick={() => setFontSize(size)}>
+              <span className="dropdown-item">{size}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <button
+          type="button"
+          className="btn btn-danger btn-sm mx-1"
+          onClick={() => setQuery('')}
+        >
+          Clear
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-success btn-sm mx-1"
+          onClick={executeQuery}
+        >
+          Run
+        </button>
+      </div>
+    </div>
+  );
+};
 
 Toolbar.propTypes = {
   fontSize: PropTypes.number.isRequired,
@@ -79,3 +75,4 @@ Toolbar.propTypes = {
   setFontSize: PropTypes.func.isRequired,
   setWordWrap: PropTypes.func.isRequired
 };
+
